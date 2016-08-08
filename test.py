439e6c71bb74830f6bd071001dd04a2971b4d37d
Sten Heinze <shze@gmx.de>
2016-08-07 21:07:09 -0400

    Add path process for test.py

--- test.py
+++ test.py
@@ -25,39 +25,81 @@ path_location = Enum('path_location', 'remote local')
 #
 class synctest_path:
   #
-  def __init__(self, location, name, type, size = None, link_dst = None):
+  def __init__(self, location, name, type, size = None, link_src = None):
     self.location = location
     self.name = name
     self.type = type
     self.size = size
-    self.link_dst = link_dst
+    self.link_src = link_src
    
 #
 class synctest:
   #
   def __init__(self, name, cmdlineparams, init_paths, sync_paths, result_paths):
     logger.debug("synctest::__init__()")
-    make_sure_dir_exists(os.path.join(name, "remote"))
-    make_sure_dir_exists(os.path.join(name, "local"))
-    
-    self.process_paths(init_paths)
-    cmdline = "python lazysync.py -r ./remote/ -l ./local/ " + cmdlineparams
-    logger.debug("synctest::__init__() cmdline='%s'", cmdline)
-    proc = subprocess.Popen(cmdline.split())
-    time.sleep(.5)
-    self.process_paths(sync_paths)
-    time.sleep(.5)
-    proc.terminate()
-    
-    # TODO cmp with result_paths
-    
-    shutil.rmtree(name)
+    self.name = name
+    self.cmdlineparams = cmdlineparams
+    self.init_paths = init_paths
+    self.sync_paths = sync_paths
+    self.result_paths = result_paths
+  
+  #
+  def process_path(self, path):
+    logger.debug("synctest::process_path()")
+    full_path = os.path.join(self.location, self.name)
+    if(path.type == path_type.file):
+      blocksize = min(path.size, 1000000) # ~1MB
+      count = path.size / blocksize; # writes exact size for <1MB file size, multiples of 1MB for larger files
+      cmdline = "dd if=/dev/zero of=" + full_path + " bs=" + blocksize + " count=" + count
+      logger.debug("synctest::process_path() cmdline='%s'", cmdline)
+      subprocess.call(cmdline.split())
+    elif(path.type == path_type.dir):
+      os.path.makedirs(full_path)
+    elif(path.type == path_type.link):
+      os.symlink(self.link_src, full_path)
+    elif(path.type == path_type.delete):
+      if(os.path.lexists(full_path)):
+        if(os.path.isdir(full_path)):
+          os.removedirs(full_path)
+        else:
+          shutil.rmtree(full_path)
+      else:
+        logger.info()("synctest::process_path() Cannot delete path '%s', it does not exist!", full_path)
   
   #
   def process_paths(self, paths):
-    pass
+    logger.debug("synctest::process_paths()")
+    for this_path in paths:
+      self.process_path(this_path)
+  
+  #
+  def paths_correct(self):
+    logger.debug("synctest::paths_correct()")
+    # TODO
+    return True
+  
+  # 
+  def run(self):
+    logger.debug("synctest::run()")
+    make_sure_dir_exists(os.path.join(self.name, "remote"))
+    make_sure_dir_exists(os.path.join(self.name, "local"))
+    
+    self.process_paths(self.init_paths)
+    cmdline = "python lazysync.py -r ./remote/ -l ./local/ " + self.cmdlineparams
+    logger.debug("synctest::run() cmdline='%s'", cmdline)
+    proc = subprocess.Popen(cmdline.split())
+    time.sleep(.5)
+    self.process_paths(self.sync_paths)
+    time.sleep(.5)
+    proc.terminate()
+    result = self.paths_correct()
+    shutil.rmtree(self.name)
+    
+    return result
   
 # main    
 if __name__ == "__main__":
   logger.debug("__main__()")
   test1 = synctest("test1", "", [], [], [])
+  test1.run()
+  # TODO
