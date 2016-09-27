700d8250616ded377bf63e125356ab36100a64ce
Sten Heinze <shze@gmx.de>
2016-09-27 15:29:47 -0400

    Implement ofnotify.notifier and needed support code.

--- ofnotify.py
+++ ofnotify.py
@@ -1,24 +1,76 @@
 #!/usr/bin/env python
 
+from collections import deque # implements atomic append() and popleft() that do not require locking
+import psutil, time, enum # enum is enum34
+
+#
+event_types = enum.Enum('event_types', 'open close')
+
+# 
+class event:
+  #
+  def __init__(self, path, event_type):
+    self.path = path
+    self.type = event_type
+
 #
 class event_processor:
   #
-  def __init__(self):
+  def process_event(self, event):
     pass
 
 #
 class notifier:
   #
-  def __init__(self):
-    pass
-  
+  def __init__(self, event_processor, watch_paths):
+    self.event_processor = event_processor
+    self.watch_paths = watch_paths
+    self.queue = deque()
+    self.tracked_files = set()
+
   #
-  def find_changes(self):
-    pass
+  def _find_changes(self):
+    # find all currently open files and create open events for files that were not open before
+    new_tracked_files = set()
+    for pid in psutil.pids():
+      try:
+        current_pid = psutil.Process(pid) # inside try-except to catch psutil.NoSuchProcess
+        for open_file in current_pid.open_files(): # catch psutil.AccessDenied
+          watched = False # only track file in watched paths
+          for watch_path in self.watch_paths:
+            if open_file.path.startswith(watch_path):
+              watched = True
+              break
+            
+          if watched:
+            new_tracked_files.add(open_file)
+      except (psutil.NoSuchProcess, psutil.AccessDenied):
+        pass
+      except:
+        raise
+      
+    # create open events for newly opened files
+    opened_files = new_tracked_files - self.tracked_files
+    for opened_file in opened_files:
+      self.queue.append(event(open_file.path, event_types.open))
+      
+    # create close events for closed files
+    closed_files = self.tracked_files - new_tracked_files
+    for closed_file in closed_files:
+      self.queue.append(event(closed_file.path, event_types.close))
+    
+    self.tracked_files = new_tracked_files # update
   
   #
   def loop(self):
-    pass
+    while 1:
+      try:
+        self._find_changes()
+        while self.queue:
+          self.event_processor.process_event(self.queue.popleft())
+        time.sleep(0.5)
+      except KeyboardInterrupt:
+        break
   
 #
 class threaded_notifier:
