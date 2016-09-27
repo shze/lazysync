671ab06221e0b47528d7ac74758e0fbc6aacc6ce
Sten Heinze <shze@gmx.de>
2016-09-27 14:13:06 -0400

    Start ofnotify, a notifier of opened and closed files.

---
+++ ofnotify.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#
+class event_processor:
+  #
+  def __init__(self):
+    pass
+
+#
+class notifier:
+  #
+  def __init__(self):
+    pass
+  
+  #
+  def find_changes(self):
+    pass
+  
+  #
+  def loop(self):
+    pass
+  
+#
+class threaded_notifier:
+  #
+  def __init__(self):
+    pass
